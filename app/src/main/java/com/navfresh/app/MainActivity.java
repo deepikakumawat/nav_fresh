500c9f5d65ee7b80658d794772ed41669d6248d8
deepikakumawat <deepika.k.kumawat@gmail.com>
2017-11-21 22:46:48 +0530

    First commit

---
+++ app/src/main/java/com/navfresh/app/MainActivity.java
@@ -0,0 +1,116 @@
+package com.navfresh.app;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final String url = "http://www.navfresh.com/";
+//    String[] PERMISSIONS = {Manifest.permission.ACCESS_NETWORK_STATE};
+//    private final int PERMISSION_ALL = 11;
+    private WebView webView;
+    private ProgressBar progressBar;
+    private LinearLayout llNetConnection;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       /* if (!hasPermissions(this, PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }*/
+        init();
+        showWebPage();
+    }
+
+    private boolean hasPermissions(Context context, String... permissions) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void init() {
+        webView = findViewById(R.id.webView);
+        progressBar = findViewById(R.id.progressBar);
+        llNetConnection = findViewById(R.id.llNetConnection);
+        llNetConnection.setOnClickListener(this);
+    }
+
+    private void showWebPage() {
+        if (isDeviceOnline()) {
+            llNetConnection.setVisibility(View.GONE);
+            webView.loadUrl(url);
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.setWebViewClient(new NavFreshWebClient());
+        } else {
+            llNetConnection.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        int vId = view.getId();
+        try {
+            switch (vId) {
+                case R.id.llNetConnection:
+                    showWebPage();
+                    break;
+                default:
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isDeviceOnline() {
+        boolean isDeviceOnLine = false;
+        try {
+            ConnectivityManager cm = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo netInfo = cm.getActiveNetworkInfo();
+            isDeviceOnLine = netInfo != null && netInfo.isConnected();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return isDeviceOnLine;
+    }
+
+    private class NavFreshWebClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+}
